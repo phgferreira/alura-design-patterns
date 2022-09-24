316853d15cae26c83c84096144921045ee321b47
Paulo Ferreira <phgferreira@outlook.com>
2022-09-24 12:55:27 -0300

    Corrigido erro no teste de imposto (main)

--- src/br/com/alura/loja/imposto/TestesImpostos.java
+++ src/br/com/alura/loja/imposto/TestesImpostos.java
@@ -7,7 +7,7 @@ import br.com.alura.loja.orcamento.Orcamento;
 public class TestesImpostos {
 
 	public static void main(String[] args) {
-		Orcamento orcamento = new Orcamento(new BigDecimal(100));
+		Orcamento orcamento = new Orcamento(new BigDecimal(100), 1);
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
 		System.out.println(calculadora.calcular(orcamento, new ICMS()));
 		System.out.println(calculadora.calcular(orcamento, new ISS()));
