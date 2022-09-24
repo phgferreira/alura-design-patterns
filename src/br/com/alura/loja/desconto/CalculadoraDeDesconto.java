d08d72676fbe3df50814143ece17fbe2a3c55bd7
Paulo Ferreira <phgferreira@outlook.com>
2022-09-24 12:56:50 -0300

    Iniciado o desenvolvimento da classe calculadora de desconto

---
+++ src/br/com/alura/loja/desconto/CalculadoraDeDesconto.java
@@ -0,0 +1,12 @@
+package br.com.alura.loja.desconto;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class CalculadoraDeDesconto {
+
+	public BigDecimal calcular(Orcamento orcamento) {
+		return BigDecimal.ZERO;
+	}
+}
